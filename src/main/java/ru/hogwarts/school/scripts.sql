82842145b60dcf6828f7c8263bd4de965030e3a0
gerchikk <geraloi@yandex.ru>
2023-10-05 14:49:45 +0500

    Внес правки после просмотра лайва

--- src/main/java/ru/hogwarts/school/scripts.sql
+++
@@ -1,5 +0,0 @@
-/* 1 */select * from student where age > 12 and age < 18;
-/* 2 */select name from student;
-/* 3 */select * from student where name like '%й%';
-/* 4 */select * from student where age < 15;
-/* 5 */select * from student order by age;
